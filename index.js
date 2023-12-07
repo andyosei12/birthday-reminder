cb30d9f7bb4eb53e15a38c26d6a44e03e5cb33e6
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-12-07 04:14:12 +0000

    add db connection

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const db = require('./db');
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+db.connect();
 
 app.get('/', (req, res) => {
   res.send('hello world');
---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  user_name: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  date_of_birth: { type: Date, required: true },
+  created_at: { type: Date, default: new Date() },
+});
+
+const UserModel = mongoose.model('users', UserSchema);
+
+module.exports = User;
