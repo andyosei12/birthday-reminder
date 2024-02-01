1affdbacbd9824b69968f91a2913635aad66e4ec
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2024-02-01 12:03:23 +0000

    update cron interval

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ db.connect();
 
 // schedule.scheduleJob('* * * * *', birthdayReminder);
 const rule = new schedule.RecurrenceRule();
-rule.hour = 11;
-rule.minute = 58;
+rule.hour = 7;
+rule.minute = 0;
 rule.tz = 'Etc/UTC';
 schedule.scheduleJob(rule, birthdayReminder);
 
